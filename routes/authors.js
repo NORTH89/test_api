5d8a70a2e571f063389b00d1edf833f7522e635f
NORTH89 <nordintata0@gmail.com>
2024-12-31 16:09:46 +0100

    add admin verification for author routes with validation and error handling

--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { Author, validateAuthor } = require('../models/Author');
-const verifyToken = require('../middleware/verifyToken');
+const {verifyAdmin} = require('../middleware/verifyToken');
+
+
 // Get all authors
+// Returns an array of authors
 router.get('/', async (req, res, next) => {
     try {
         const authors = await Author.find();
@@ -23,8 +26,20 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
-// Create an author
-router.post('/', async (req, res, next) => {
+/**
+ * Create an author
+ * Only admin can create an author
+ * We'll check for the admin token in the verifyAdmin middleware
+ * We'll also validate the author input
+ * If the input is valid, we'll create the author
+ * If the input is invalid, we'll return a 400 status
+ * If there's an error, we'll pass it to the error handler
+ * @param {String} name - The author name
+ * @param {String} bio - The author bio
+ * @returns {Object} - The created author
+ * @throws {Object} - The error object
+ */
+router.post('/', verifyAdmin, async (req, res, next) => {
     try {
         const { error } = validateAuthor(req.body);
         if (error) return res.status(400).json(error.details[0].message);
@@ -40,8 +55,19 @@ router.post('/', async (req, res, next) => {
     }
 });
 
-// Update an author
-router.put('/:id', async (req, res, next) => {
+/**
+ * Update an author
+ * Only admin can update an author
+ * We'll check for the admin token in the verifyAdmin middleware
+ * We'll also check if the author exists
+ * If the author exists, we'll update the author
+ * If the author doesn't exist, we'll return a 404 status
+ * If there's an error, we'll pass it to the error handler
+ * @param {String} id - The author ID
+ * @returns {Object} - The updated author
+ * @throws {Object} - The error object
+ */
+router.put('/:id',verifyAdmin, async (req, res, next) => {
     try {
         const { error } = validateAuthor(req.body);
         if (error) return res.status(400).json(error.details[0].message);
@@ -58,8 +84,19 @@ router.put('/:id', async (req, res, next) => {
     }
 });
 
-// Delete an author
-router.delete('/:id', async (req, res, next) => {
+/**
+ * Delete an author
+ * Only admin can delete an author
+ * We'll check for the admin token in the verifyAdmin middleware
+ * We'll also check if the author exists
+ * If the author exists, we'll delete the author
+ * If the author doesn't exist, we'll return a 404 status
+ * If there's an error, we'll pass it to the error handler
+ * @param {String} id - The author ID
+ * @returns {Object} - The deleted author
+ * @throws {Object} - The error object
+ */
+router.delete('/:id',verifyAdmin, async (req, res, next) => {
     try {
         const author = await Author.findByIdAndRemove(req.params.id);
         if (!author) return res.status(404).json('Author not found');
