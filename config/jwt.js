4eb72d040f420ef3adc845e1480188c5b7cc7411
NORTH89 <nordintata0@gmail.com>
2025-01-02 12:57:36 +0100

    comment out JWT token generation code

--- config/jwt.js
+++ config/jwt.js
@@ -1,14 +1,13 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const generateToken = (user) => {
-    return jwt.sign(
-        {
-            id: user._id,
-            isAdmin: user.isAdmin
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: '1h' }
-    );
-};
+// const generateToken = (user) => {
+//     return jwt.sign(
+//         {
+//             id: user._id,
+//             isAdmin: user.isAdmin
+//         },
+//         process.env.JWT_SECRET,
+//     );
+// };
 
-module.exports = { generateToken };
+// module.exports = { generateToken };
