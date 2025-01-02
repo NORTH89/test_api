d0af584ac32041944a66c1cd82c4d03c00b57bc2
NORTH89 <nordintata0@gmail.com>
2025-01-02 12:57:17 +0100

    refactor app.js to establish MongoDB connection

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { notFound, logger, errorHandler } = require('./middleware/errorHandler');
-const authRouter = require('./routes/auth');
-const registerRouter = require('./routes/auth');
-const loginRouter = require('./routes/auth');
-const booksRouter = require('./routes/books');
-const authorsRouter = require('./routes/authors');
-const usersRouter = require('./routes/users');
+const connectToMongoDB = require('./config/mongodb');
+
 dotenv.config();
 
 const app = express();
 
 
 app.use(express.json());
-app.use('/api/books', booksRouter);
-app.use('/api/authors', authorsRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/register', registerRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/users', usersRouter);
+app.use('/api/books', require('./routes/books'));
+app.use('/api/authors', require('./routes/authors'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/register', require('./routes/auth'));
+app.use('/api/login', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
 
+// Set the port number
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true, // Add this line to fix the deprecation warning
-    useFindAndModify: false // Add this to prevent other deprecation warnings
-})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
-
+connectToMongoDB();
 
 // Middleware for handling 404 errors
 app.use(notFound);
