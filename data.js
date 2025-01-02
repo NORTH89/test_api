55c9c84951567e20b51faa14f5171ca15ef108de
NORTH89 <nordintata0@gmail.com>
2025-01-02 12:57:25 +0100

    add initial book data to the database

---
+++ data.js
@@ -0,0 +1,26 @@
+const books = [
+    {
+        title: "The Art of Programming",
+        author: "6769533d454b1d16b0f43c21",
+        description: "A comprehensive guide to modern programming practices",
+        price: 29.99,
+        language: "English",
+    },
+    {
+        title: "Data Structures Explained",
+        author: "6769533d454b1d16b0f43c21",
+        description: "Learn data structures from basics to advanced",
+        price: 34.99,
+        language: "English",
+    },
+    {
+        title: "Web Development Mastery",
+        author: "6768733002cdb71468d2d7ef",
+        description: "Modern web development techniques and practices",
+        price: 39.99,
+        language: "English",
+        
+    }
+];
+
+module.exports = books;
